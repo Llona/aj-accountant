719ecaef2cc6cea160dcd4fe4e99719a974942c1
Llona <liu.lon@gmail.com>
2018-04-02 12:00:43 +0800

    1. still in test progress 2. add gen account list function

--- aj-accountant.py
+++ aj-accountant.py
@@ -9,18 +9,23 @@ import const_define
 # print(workbook.active)
 
 # load excel file
-workbook = openpyxl.load_workbook(const_define.DETAILED_LEDGER_FULL_PATH)
+workbook = openpyxl.load_workbook(const_define.DETAILED_LEDGER_FULL_PATH, data_only=True)
 # get all sheet name
-worksheets = workbook.get_sheet_names()
+# worksheets = workbook.get_sheet_names()
+worksheets = tuple(workbook.sheetnames)
+print(worksheets)
+
+for i in worksheets:
+    print(i)
 
 # get sheet content
-sheet = workbook.get_sheet_by_name(worksheets[0])
+sheet = workbook[worksheets[0]]
 # print(sheet)
 # print(sheet.title)
 # print(sheet.cell(row=2, column=2).value)
+# print(sheet['J2'].value)
 
-for rowOfCell in sheet['B2':'G2']:
-    print(rowOfCell)
+for rowOfCell in sheet['B2':'M2']:
     for cell in rowOfCell:
         # print(cell.coordinate, cell.value)
         print(cell.value)
@@ -30,8 +35,8 @@ for rowOfCell in sheet['B2':'G2']:
 # filename_netdriver = os.path.join(filename_netdriver, 'Roy')
 # filename_netdriver = os.path.join(filename_netdriver, 'command.txt')
 # print(filename_netdriver)
-# shutil.copy(filename_netdriver, os.getcwd())
+# shutil.copy(filename_netdriver, DATA_FOLDER_FULL_PATH)
 
-filename_netdriver = os.path.join(r"t:", r'vv\Roy\command.txt')
-print(filename_netdriver)
-shutil.copy(filename_netdriver, os.getcwd())
+# filename_netdriver = os.path.join(r"t:", r'vv\Roy\command.txt')
+# print(filename_netdriver)
+# shutil.copy(filename_netdriver, DATA_FOLDER_FULL_PATH)
--- const_define.py
+++ const_define.py
@@ -1,5 +1,8 @@
+# -*- coding: UTF-8 -*-
 import os
 
+
 DATA_FOLDER_NAME = 'data'
-DETAILED_LEDGER_FILENAME = 'Check-In list_william.xlsx'
-DETAILED_LEDGER_FULL_PATH = os.path.join(os.path.join(os.getcwd(), DATA_FOLDER_NAME), DETAILED_LEDGER_FILENAME)
+DETAILED_LEDGER_FILENAME = r'107年方殷營業支出.xlsx'
+DATA_FOLDER_FULL_PATH = os.path.join(os.getcwd(), DATA_FOLDER_NAME)
+DETAILED_LEDGER_FULL_PATH = os.path.join(DATA_FOLDER_FULL_PATH, DETAILED_LEDGER_FILENAME)
